41eb874d7ccd665fc553a74dc7942d508059b001
appledcrab <124226900+appledcrab@users.noreply.github.com>
2024-05-14 20:26:29 -0400

    Changes the way buttons made.. in html instead of in js.

--- odin-js-practice/rock-paper-sissors/rock_paper_scissors.js
+++ odin-js-practice/rock-paper-sissors/rock_paper_scissors.js
@@ -4,6 +4,10 @@ Project for The Odin Project.
 Changing to have ui using previous code
 */
 const button_container = document.querySelector("#button-container");
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+
 
 const playButton = document.querySelector('#play-button');
 playButton.addEventListener('click', playGame);
@@ -12,37 +16,6 @@ playButton.style.color = "white";
 playButton.style.padding = "10px";
 playButton.style.fontSize = "20px";
 
-// makes buttons and hides them for now
-const rockButton = document.createElement("button");
-rockButton.textContent = "Rock";
-rockButton.style.backgroundColor = "grey";
-rockButton.style.color = "white";
-rockButton.style.margin = "10px";
-rockButton.style.padding = "10px";
-rockButton.style.fontSize = "20px";
-button_container.appendChild(rockButton);
-
-const paperButton = document.createElement("button");
-paperButton.textContent = "Paper";
-paperButton.style.backgroundColor = "grey";
-paperButton.style.color = "white";
-paperButton.style.margin = "10px";
-paperButton.style.padding = "10px";
-paperButton.style.fontSize = "20px";
-button_container.appendChild(paperButton);
-
-const scissorsButton = document.createElement("button");
-scissorsButton.textContent = "Scissors";
-scissorsButton.style.backgroundColor = "grey";
-scissorsButton.style.color = "white";
-scissorsButton.style.margin = "10px";
-scissorsButton.style.padding = "10px";
-scissorsButton.style.fontSize = "20px";
-button_container.appendChild(scissorsButton);
-
-rockButton.style.display = "none";
-paperButton.style.display = "none";
-scissorsButton.style.display = "none";
 
 const resultContainer = document.querySelector("#result-container");
 
