ecde6e6c966b2643c19813a10990a174b7b4138f
appledcrab <124226900+appledcrab@users.noreply.github.com>
2024-05-31 00:30:10 -0400

    Completes minimum of TOP's "Etch-A-Sketch" Project -Done with bootstrap! as apart of me trying to explore it more .No custom colors yet, not fully sure how to do that properly

---
+++ odin-js-practice/etch-a-sketch/script.js
@@ -0,0 +1,64 @@
+// 16 by 16 grid of square divs for the etch-a-sketch
+
+// Create a 16x16 grid of square divs
+const gridSize = 16;
+createGrid(gridSize);
+
+
+
+function createGrid(gridSize) {
+    // const container = document.querySelector('#container');
+    const body = document.getElementById('body');
+    const container = document.createElement('div');
+    container.id = 'container';
+
+    container.classList.add('d-flex','flex-column','align-items-center','justify-content-center','m-5','p-5');
+    console.log('Creating grid');
+    for(let i = 0; i< gridSize; i++){
+        const div = document.createElement('div');
+        div.classList.add('bg-light');
+        div.classList.add('border','d-flex','flex-row','justify-content-center');
+
+        for(let j = 0; j < gridSize; j++){
+            const item = document.createElement('div');
+            item.classList.add('grid-item');
+            item.classList.add('p-1','border','border-red','p-2');
+            div.appendChild(item);
+            
+            // event listener for hover effect
+            item.addEventListener('mouseover', (e) => {
+                item.style.backgroundColor = 'black';
+            });
+            item.addEventListener('dblclick', () =>{
+                item.style.backgroundColor = 'white';
+            
+            })
+            
+            // console.log('Grid item created');
+        }
+        container.appendChild(div);
+    }
+    body.appendChild(container);
+}
+
+function clearGrid(){
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => item.style.backgroundColor = 'white');
+}
+
+function changeSize(){
+    const size = prompt("Change the size? (default 16)");
+    if (size == null){
+        size = 16
+    }
+
+    // remove previous grid
+    const container = document.getElementById('container');
+    container.remove();
+
+    // then readd by doing create grid again
+    createGrid(size);
+
+    
+}
+
